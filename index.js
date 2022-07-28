5447fbf485f42583a9c642068fd31f4646e3b74b
Ajay16777 <ajaysingh16777208@gmail.com>
2022-07-28 18:22:35 +0530

    test routes1

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Connect to MongoDB
 mongooes
-  .connect("mongodb://localhost/BounceBox", { useNewUrlParser: true })
+  .connect("mongodb+srv://adit:adit@cluster0.azqba7k.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })

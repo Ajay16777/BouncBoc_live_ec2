de831e954e0a5f8785fec8f9ee8cc192c76a6f75
Ajay16777 <ajaysingh16777208@gmail.com>
2022-07-28 12:30:46 +0530

    test routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ip = require("ip");
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -27,6 +27,46 @@ mongooes
       console.log(`http://localhost:${port}`);
     });
   });
+
+
+  //test connection
+  app.get("/", (req, res) => {
+    //all routes info here
+
+   let routes = [
+    {
+      method: "GET",
+      path: "/",
+      description: "Homepage",
+      response: "Hello World",
+    },
+    {
+      method: "GET",
+      path: "/api/v1/projects",
+      description: "Get all projects",
+      response: "Array of projects",
+    },
+    {
+      method: "GET",
+      path: "/api/v1/projects/:id",
+      description: "Get project by id",
+      response: "Project",
+    },
+    {
+      method: "POST",
+      path: "/api/v1/projects",
+      description: "Create a new project",
+      response: "Project",
+    },
+  ];
+
+  res.send(routes);
+
+
+    
+  }
+  );
+
 
 app.use("/api/users", require("./routes/User"));
 app.use("/api/projects", require("./routes/Project"));
